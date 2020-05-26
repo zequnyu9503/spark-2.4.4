819fc9f9321fba77d1c1945ff442deb69192b525
zequnyu9503 <greatyuzequn@outlook.com>
2020-05-27 07:39:07 +0800

    test function "retrieve executors' free storage memory".

---
+++ run/twitter.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+spark-submit \
+--master spark://centos3:7079 \
+--executor-memory 12g \
+--executor-cores 4 \
+--driver-cores 4 \
+--driver-memory 12g \
+../examples/target/original-spark-examples_2.11-2.4.4.jar
