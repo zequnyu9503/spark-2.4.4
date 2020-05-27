ea4a60b115cdf5b9b199c5c532d711a3a2ad6dca
zequnyu9503 <greatyuzequn@outlook.com>
2020-05-27 20:36:21 +0800

    fix shell.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 MASTER_URL="spark://centos3:7079"
 SPARK_HOME="/home/zc/service/spark-2.4.4"
-slaves=("centos3" "centos4" "centos5" "centos11" "centos12" "centos13")
+slaves=("centos4" "centos5" "centos11" "centos12" "centos13")
 if [[ $(hostname) = "centos3" ]];then
 echo "start..."
 sh "${SPARK_HOME}/sbin/start-master.sh"
