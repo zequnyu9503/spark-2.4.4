03578816d06147192b2ea5d6725836e324488bee
zequnyu9503 <greatyuzequn@outlook.com>
2020-05-27 19:02:13 +0800

    add shells.

---
+++ compile-cae.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#host=$(hostname)
+#if [[ $(hostname) = "centos3" ]];then
+#sh halt.sh
+#fi
+#git pull
+#mvn clean install -DskipTests -pl core,assembly,examples
+slaves=("centos3" "centos4" "centos5" "centos11" "centos12" "centos13")
+for slave in ${slaves[@]}
+ do
+  ssh "zc@${slave}" "cd /home/zc/service/spark-2.4.4/;git pull;mvn clean install -DskipTests -pl core,assembly,examples" &
+ done
+wait
--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-host=$(hostname)
-if [[ $(hostname) = "centos3" ]];then
-sh halt.sh
-fi
-git pull
-mvn clean install -DskipTests -pl core,assembly,examples
