0d1f2ca1a7952af24592edcfc1b64a2dfe3f24cb
zequnyu9503 <greatyuzequn@outlook.com>
2020-05-27 18:54:16 +0800

    add shell.

---
+++ newest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+slaves=("centos3" "centos4" "centos5" "centos11" "centos12" "centos13")
+for slave in ${slaves[@]}
+ do
+  ssh "zc@${slave}" "cd /home/zc/service/spark-2.4.4/;git pull" &
+ done
