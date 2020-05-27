e5b8dd6e77f8aff5d08498b4978115c3b02b4458
zequnyu9503 <greatyuzequn@outlook.com>
2020-05-27 10:21:02 +0800

    add rddmemsize experiment.

--- run/rddmemorysize.sh
+++ run/rddmemorysize.sh
@@ -5,5 +5,6 @@ spark-submit \
 --executor-cores 4 \
 --driver-cores 4 \
 --driver-memory 12g \
---class org.apache.spark.examples.tw.Twitter \
-examples/target/original-spark-examples_2.11-2.4.4.jar
+--class org.apache.spark.examples.tw.RDDMemorySize \
+examples/target/original-spark-examples_2.11-2.4.4.jar \
+/home/zc/yzq/download/rdd_memory_size.txt
