5be8cd6b7288a264f75df36fe8aea26ec329732a
zequnyu9503 <greatyuzequn@outlook.com>
2020-05-29 09:21:38 +0800

    更新TW API & Prefetch.

--- compile-core.sh
+++ compile-core.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 slaves=("centos3" "centos4" "centos5" "centos11" "centos12" "centos13")
+sh halt.sh
 for slave in ${slaves[@]}
  do
   ssh "zc@${slave}" "cd /home/zc/service/spark-2.4.4/;git pull;mvn clean install -DskipTests -pl core,assembly" &
--- compile-examples.sh
+++ compile-examples.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+sh halt.sh
 git pull
 mvn clean install -DskipTests -pl examples
