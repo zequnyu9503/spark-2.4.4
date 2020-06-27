e693b2ccbcf61ef545480ce9508eb5f4e2f96332
zequnyu9503 <greatyuzequn@outlook.com>
2020-06-28 07:41:26 +0800

    Update workloads.

--- run/twitter.sh
+++ run/twitter.sh
@@ -5,11 +5,11 @@ rm /home/zc/service/spark-2.4.4/logs/*
 spark-submit \
 --master spark://centos3:7079 \
 --executor-memory 16g \
---executor-cores 3 \
+--executor-cores 1 \
 --driver-cores 2 \
 --driver-memory 16g \
 --class org.apache.spark.examples.tw.Twitter \
 --driver-java-options "-Dlog4j.configuration=file:${log_path}" \
 --jars ${libs_dir}/fastjson-1.2.35.jar \
 examples/target/original-spark-examples_2.11-2.4.4.jar \
-true 1 10
+false 1 10
